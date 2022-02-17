155563ff15d78e863e32de56df33e20c59929b33
Ivandro Neto <ivandroneto1@hotmail.com>
2022-02-17 23:50:00 +0530

    Add Mobile Layout and behavior

--- JS/behavior.js
+++ JS/behavior.js
@@ -100,7 +100,82 @@ let list = [{
       }
     }
   }
-];
+]
+let actived = "" 
+document.querySelector("#tw").innerHTML= list[0].title
+document.querySelector("#tst").innerHTML= list[1].title
+document.querySelector("#tpl").innerHTML= list[2].title
+document.querySelector("#tex").innerHTML= list[3].title
+document.querySelector("#tso").innerHTML= list[4].title
+document.querySelector("#tse").innerHTML= list[5].title
+actived = "weekly"
+function over(x) {if(x.style.color != "white" ){x.style.color = "white"}}
+function out(x) {if (x.style.color == "white" && actived != x.id){x.style.color = "hsl(236, 100%, 87%)"}}
+function changeToDayly(){
+  actived = "daily"
+  select(actived)
+}
+function changeToWeekly() {
+  actived = "weekly"
+  select(actived)
+}
+function changeToMonthly() {
+  actived = "monthly"
+  select(actived)
+}
+function select(_actived){
+if(_actived == "daily"){
+  document.querySelector("#cw").innerHTML = list[0].timeframes.daily.current + 'hrs'
+  document.querySelector("#pw").innerHTML = 'Last day - ' + list[0].timeframes.daily.previous + 'hrs'
+      document.querySelector("#cst").innerHTML = list[1].timeframes.daily.current + 'hrs'
+      document.querySelector("#pst").innerHTML = 'Last day - ' + list[1].timeframes.daily.previous + 'hrs'
+      document.querySelector("#cpl").innerHTML = list[2].timeframes.daily.current + 'hrs'
+      document.querySelector("#ppl").innerHTML = 'Last day - ' + list[2].timeframes.daily.previous + 'hrs'
+      document.querySelector("#cex").innerHTML = list[3].timeframes.daily.current + 'hrs'
+      document.querySelector("#pex").innerHTML = 'Last day - ' + list[3].timeframes.daily.previous + 'hrs'
+      document.querySelector("#cso").innerHTML = list[4].timeframes.daily.current + 'hrs'
+      document.querySelector("#pso").innerHTML = 'Last day - ' + list[4].timeframes.daily.previous + 'hrs'
+      document.querySelector("#cse").innerHTML = list[5].timeframes.daily.current + 'hrs'
+      document.querySelector("#pse").innerHTML = 'Last day - ' + list[5].timeframes.daily.previous + 'hrs'
+      document.querySelector("#daily").style.color = "white"; 
+      document.querySelector("#weekly").style.color = " hsl(236, 100%, 87%)";
+      document.querySelector("#monthly").style.color = " hsl(236, 100%, 87%)";
+    }
 
-let work = document.querySelector("tw");
-work.innerHTML = "nome";
+    if(_actived == "weekly"){
+      document.querySelector("#cw").innerHTML = list[0].timeframes.weekly.current + 'hrs'
+      document.querySelector("#pw").innerHTML = 'Last week - ' + list[0].timeframes.weekly.previous + 'hrs'
+      document.querySelector("#cst").innerHTML = list[1].timeframes.weekly.current + 'hrs'
+      document.querySelector("#pst").innerHTML = 'Last week - ' + list[1].timeframes.weekly.previous + 'hrs'
+      document.querySelector("#cpl").innerHTML = list[2].timeframes.weekly.current + 'hrs'
+      document.querySelector("#ppl").innerHTML = 'Last week - ' + list[2].timeframes.weekly.previous + 'hrs'
+      document.querySelector("#cex").innerHTML = list[3].timeframes.weekly.current + 'hrs'
+      document.querySelector("#pex").innerHTML = 'Last week - ' + list[3].timeframes.weekly.previous + 'hrs'
+      document.querySelector("#cso").innerHTML = list[4].timeframes.weekly.current + 'hrs'
+      document.querySelector("#pso").innerHTML = 'Last week - ' + list[4].timeframes.weekly.previous + 'hrs'
+      document.querySelector("#cse").innerHTML = list[5].timeframes.weekly.current + 'hrs'
+      document.querySelector("#pse").innerHTML = 'Last week - ' + list[5].timeframes.weekly.previous + 'hrs'
+      document.querySelector("#daily").style.color = " hsl(236, 100%, 87%)"; 
+      document.querySelector("#weekly").style.color = "white";
+      document.querySelector("#monthly").style.color = " hsl(236, 100%, 87%)";
+    }
+  
+      if(_actived == "monthly"){
+        document.querySelector("#cw").innerHTML = list[0].timeframes.monthly.current + 'hrs'
+        document.querySelector("#pw").innerHTML = 'Last month - ' + list[0].timeframes.monthly.previous + 'hrs'
+        document.querySelector("#cst").innerHTML = list[1].timeframes.monthly.current + 'hrs'
+        document.querySelector("#pst").innerHTML = 'Last month - ' + list[1].timeframes.monthly.previous + 'hrs'
+        document.querySelector("#cpl").innerHTML = list[2].timeframes.monthly.current + 'hrs'
+        document.querySelector("#ppl").innerHTML = 'Last month - ' + list[2].timeframes.monthly.previous + 'hrs'
+        document.querySelector("#cex").innerHTML = list[3].timeframes.monthly.current + 'hrs'
+        document.querySelector("#pex").innerHTML = 'Last month - ' + list[3].timeframes.monthly.previous + 'hrs'
+        document.querySelector("#cso").innerHTML = list[4].timeframes.monthly.current + 'hrs'
+        document.querySelector("#pso").innerHTML = 'Last month - ' + list[4].timeframes.monthly.previous + 'hrs'
+        document.querySelector("#cse").innerHTML = list[5].timeframes.monthly.current + 'hrs'
+        document.querySelector("#pse").innerHTML = 'Last month - ' + list[5].timeframes.monthly.previous + 'hrs'
+        document.querySelector("#daily").style.color = " hsl(236, 100%, 87%)"; 
+        document.querySelector("#weekly").style.color = "hsl(236, 100%, 87%)";
+        document.querySelector("#monthly").style.color = "white";
+      }
+    }
+    select(actived)
